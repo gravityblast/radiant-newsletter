ffd4679dbf808ccacf0e37dcf8fce19f7068c534
Andrea Franz <andrea@gravityblast.com>
2008-01-23 15:59:54 +0000

    Refactoring
    git-svn-id: http://dev.gravityblast.com/svn/projects/radiant/extensions/newsletter@76 f0b21ff1-402e-e14f-a5c2-e6dc25fb685e

--- app/controllers/newsletter_logs_controller.rb
+++ app/controllers/newsletter_logs_controller.rb
@@ -1,7 +1,7 @@
 class NewsletterLogsController < ApplicationController
 
   def index    
-    @logs = NewsletterLog.find(:all, :order => 'newsletter_logs.created_at DESC', :limit => 50)
+    @logs = NewsletterLog.find(:all, :order => 'newsletter_logs.created_at DESC, newsletter_logs.id DESC ', :limit => 50)
   end
   
 end
